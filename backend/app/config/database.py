4e37c9c998ede3ef32835a52f132ccdcfe8082a3
Victor <victorhenrique.alves.lopes@gmail.com>
2025-03-27 16:29:01 -0300

    Simplificando a inicialização de uma instância do SQLAlchemy

--- backend/app/config/database.py
+++ backend/app/config/database.py
@@ -1,10 +1,3 @@
 from flask_sqlalchemy import SQLAlchemy
-from config.config import Config
 
-db = SQLAlchemy() # Cria a instância do SQLAlchemy
-
-def init_db(app):
-    """Configura e inicializa o banco de dados com a aplicação Flask."""
-    app.config["SQLALCHEMY_DATABASE_URI"] = Config.SQLALCHEMY_DATABASE_URI
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = Config.SQLALCHEMY_TRACK_MODIFICATIONS
-    db.init_app(app)
+db = SQLAlchemy()
