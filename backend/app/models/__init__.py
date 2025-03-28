4c689dc4163729c1baff22f9eba25ca8ab7ef5dc
Victor <victorhenrique.alves.lopes@gmail.com>
2025-03-28 18:08:47 -0300

    Incluindo os novos modelos no '__init__.py'

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,5 +1,7 @@
 from .model_aluno import Aluno
 from .model_professor import Professor
 from .model_materia import Materia
+from .model_documento import Documento
 from .model_aluno_materia import AlunoMateria
 from .model_professor_materia import ProfessorMateria
+from .model_documento_materia import DocumentoMateria
