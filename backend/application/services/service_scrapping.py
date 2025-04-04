1bbf4efc52a63bcbe6d73c3b4ff13bd6d0e758c7
Gustavo Drumond <gdrumondduarte@gmail.com>
2025-04-04 15:47:23 -0300

    Implementando extração de urls de imagens no arquivo service_scrapping.py

--- backend/application/services/service_scrapping.py
+++ backend/application/services/service_scrapping.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from urllib.parse import urlparse
 import sys
 import io
 import time
@@ -12,21 +13,59 @@ sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 def configure_browser():
     """
     Configura o navegador com opções de headless e idioma.
-    """
-    
+    """    
     browser_options = Options()
-    browser_options.add_argument('--headless') # Executar o navegador sem interface gráfica
-    browser_options.add_argument('--disable-notifications') # Desabilitar notificação
-    browser_options.add_argument('--lang=pt-BR') # Definir o idioma do navegador
+    browser_options.add_argument('--headless')
+    browser_options.add_argument('--disable-notifications')
+    browser_options.add_argument('--lang=pt-BR')
 
     service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service, options=browser_options)
-    return driver
+    return webdriver.Chrome(service=service, options=browser_options)
+     
+     
+     
+"""
+Devido aos erros de sintaxe gerados pelo scrapping, a funcao text_normalization foi criada para normalizar os textos
+"""    
+def text_normalization(text):
+    return text.encode('utf-8', errors='ignore').decode('utf-8').strip() if text else ""
 
+def extract_title(driver):
+    titles = []
+    for tag in ['h1', 'h2', 'h3', 'h4', 'h5', 'h6']:
+        elements = driver.find_elements(By.TAG_NAME, tag)
+        for element in elements:
+            tittle_text = text_normalization(element.text)
+            if tittle_text and not tittle_text.isspace():
+                titles.append(tittle_text)
+    return titles
 
+def extract_paragraphs(driver):
+    paragraphs = []
+    elements = driver.find_elements(By.TAG_NAME, 'p')
+    for element in elements:
+        text = text_normalization(element.text)
+        if text and not text.isspace():
+            paragraphs.append(text)
+    return paragraphs
+
+def extract_links(driver):
+    links = []
+    elements = driver.find_elements(By.TAG_NAME, 'a')
+    for element in elements:
+        links.append(element.get_attribute('href'))
+    return links
+
+def extract_images(driver):
+    images = []
+    elements = driver.find_elements(By.TAG_NAME, 'img')
+    for element in elements:
+        images.append(element.get_attribute('src'))
+    return images
 
 def data_extraction(driver, url):
     try :
+        print(f'Extraindo dados da URL: {url}')
         driver.get(url)
         time.sleep(2)
         
@@ -39,57 +78,23 @@ def data_extraction(driver, url):
                     }
                 """)
         
-        """
-        titulo_pagina armazena o titulo da pagina
-        url armazena a url da pagina
-        titulos armazena os titulos encontrados na pagina
-        paragrafos armazena os paragrafos encontrados na pagina
-        """
-        
         data = {
             'url': url,
             'page_title': driver.title,
-            'titles': [],
-            'paragraphs': [],
-            'links': []
+            'titles': extract_title(driver),
+            'paragraphs': extract_paragraphs(driver),
+            'links': extract_links(driver),
+            'images': extract_images(driver),
         }
-    
         
-        # funcao para normalizar o texto
-        def text_normalization(text):
-            return text.encode('utf-8', errors='ignore').decode('utf-8').strip()
-        
-        def extract_title():
-            titles = []
-            for tag in ['h1', 'h2', 'h3', 'h4', 'h5', 'h6']:
-                elements = driver.find_elements(By.TAG_NAME, tag)
-                for element in elements:
-                    titles.append(text_normalization(element.text))
-            return titles
-        
-        def extract_paragraphs():
-            paragraphs = []
-            elementos = driver.find_elements(By.TAG_NAME, 'p')
-            for element in elementos:
-                text = text_normalization(element.text)
-                if text and not text.isspace():
-                    paragraphs.append(text)
-            return paragraphs
-        
-        def extract_links():
-            links = []
-            elements = driver.find_elements(By.TAG_NAME, 'a')
-            for element in elements:
-                links.append(element.get_attribute('href'))
-            return links
-        
-        data['titles'] = extract_title()
-        data['paragraphs'] = extract_paragraphs()
-        data['links'] = extract_links()
-                       
+        print(f'Dados extraídos com sucesso da URL: {url}')             
         return data
         
     except Exception as e:
-        print(f'Ocorreu um erro: {str(e)}')
-        return None
+        print(f'Erro ao extrair {url}: {str(e)}')
+        return {
+            'url': url,
+            'status': 'error',
+            'error_message': str(e)
+        }
     
