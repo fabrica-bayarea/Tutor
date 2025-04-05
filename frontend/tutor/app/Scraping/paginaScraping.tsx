b69a1431f033d3e0d2f3e92ed004840b7ed05d07
Ivin <vaz.ivin@gmail.com>
2025-04-05 02:38:59 -0300

    Adicionando parte da interface de usuário.

--- frontend/tutor/app/Scraping/paginaScraping.tsx
+++ frontend/tutor/app/Scraping/paginaScraping.tsx
@@ -1,11 +1,10 @@
 import Image from "next/image";
-import styles from "./estilosScraping.module.css";
-
+import React from "react";
 
 export default function PaginaScraping() {
   return (
-    <div>
-      <h1>Página de Scraping</h1>
+    <div className="container">
+
     </div>
   );
 }
--- frontend/tutor/app/page.tsx
+++ frontend/tutor/app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
-import styles from "./page.module.css";
 import PaginaExtracao from "./Extracao/paginaExtracao";
 import PaginaChat from "./Chat/paginaChat";
+import React from "react";
 
 export default function Home() {
   return (
