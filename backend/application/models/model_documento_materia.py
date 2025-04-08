03d9c17d81a23fc24e8be3c83b62d0750a57ba8a
Victor <victorhenrique.alves.lopes@gmail.com>
2025-04-08 19:59:06 -0300

    Deixando de usar IDs únicos nas tabelas intermediárias, e transformando as FKs em PKs compostas

--- backend/application/models/model_documento_materia.py
+++ backend/application/models/model_documento_materia.py
@@ -1,13 +1,13 @@
 from application.config import db
-import uuid
 
 class DocumentoMateria(db.Model):
-    """Entidade intermediária, responsável por lidar com relacionamentos entre Documentos e Matérias."""
+    """
+    Entidade intermediária, responsável por lidar com relacionamentos entre Documentos e Matérias.
+    """
     __tablename__ = 'documentos_materias'
 
-    id = db.Column(db.UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    documento_id = db.Column(db.UUID(as_uuid=True), db.ForeignKey('documentos.id'), nullable=False)
-    materia_id = db.Column(db.UUID(as_uuid=True), db.ForeignKey('materias.id'), nullable=False)
+    documento_id = db.Column(db.UUID(as_uuid=True), db.ForeignKey('documentos.id'), nullable=False, primary_key=True)
+    materia_id = db.Column(db.UUID(as_uuid=True), db.ForeignKey('materias.id'), nullable=False, primary_key=True)
 
     documento = db.relationship('Documento', backref='materias', foreign_keys=[documento_id])
     materia = db.relationship('Materia', backref='documentos', foreign_keys=[materia_id])
--- backend/application/models/model_professor_materia.py
+++ backend/application/models/model_professor_materia.py
@@ -1,13 +1,13 @@
 from application.config import db
-import uuid
 
 class ProfessorMateria(db.Model):
-    """Entidade intermediária, responsável por lidar com relacionamentos entre Professores e Matérias."""
+    """
+    Entidade intermediária, responsável por lidar com relacionamentos entre Professores e Matérias.
+    """
     __tablename__ = 'professores_materias'
 
-    id = db.Column(db.UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    professor_id = db.Column(db.UUID(as_uuid=True), db.ForeignKey('professores.id'), nullable=False)
-    materia_id = db.Column(db.UUID(as_uuid=True), db.ForeignKey('materias.id'), nullable=False)
+    professor_id = db.Column(db.UUID(as_uuid=True), db.ForeignKey('professores.id'), nullable=False, primary_key=True)
+    materia_id = db.Column(db.UUID(as_uuid=True), db.ForeignKey('materias.id'), nullable=False, primary_key=True)
 
     professor = db.relationship('Professor', backref='materias', foreign_keys=[professor_id])
     materia = db.relationship('Materia', backref='professores', foreign_keys=[materia_id])
