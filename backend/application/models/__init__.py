23c9e6e9c7cac9b90d21a4eb92e13e9b96e59d9c
Victor <victorhenrique.alves.lopes@gmail.com>
2025-04-08 20:00:56 -0300

    Atualizando o '__init__.py' da pasta 'models'

--- backend/application/models/__init__.py
+++ backend/application/models/__init__.py
@@ -1,8 +1,9 @@
 from .model_aluno import Aluno
 from .model_professor import Professor
+from .model_turma import Turma
 from .model_materia import Materia
 from .model_documento import Documento
-from .model_aluno_materia import AlunoMateria
+from .model_aluno_turma import AlunoTurma
+from .model_turma_materia import TurmaMateria
 from .model_professor_materia import ProfessorMateria
 from .model_documento_materia import DocumentoMateria
-
