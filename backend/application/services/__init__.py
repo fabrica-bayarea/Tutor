f2768774d4ad3ac60572d43674e1698dc2d1c6fa
Victor <victorhenrique.alves.lopes@gmail.com>
2025-04-18 18:20:25 -0300

    Adicionando novos serviços ao '__init__.py'

--- backend/application/services/__init__.py
+++ backend/application/services/__init__.py
@@ -1,2 +1,6 @@
+from .service_materia import *
+from .service_professor import *
+from .service_turma import *
 from .service_arquivo import *
 from .service_scrapping import *
+from .service_vinculos import *
