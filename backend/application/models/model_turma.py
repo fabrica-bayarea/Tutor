59961f724302569f4c31d051079ca06f1747d025
Victor <victorhenrique.alves.lopes@gmail.com>
2025-04-18 18:36:24 -0300

    Corrigindo quantidade de caracteres das colunas 'semestre' e 'turno' no modelo de Turma, e gerando uma nova migração para isso

--- backend/application/models/model_turma.py
+++ backend/application/models/model_turma.py
@@ -6,8 +6,8 @@ class Turma(db.Model):
 
     id = db.Column(db.UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     codigo = db.Column(db.String(8), nullable=False, unique=True, index=True)
-    semestre = db.Column(db.String(8), nullable=False)
-    turno = db.Column(db.String(6), nullable=False)
+    semestre = db.Column(db.String(6), nullable=False)
+    turno = db.Column(db.String(10), nullable=False)
 
     alunos_matriculados = db.relationship('AlunoTurma', back_populates='turma', cascade='all, delete-orphan')
     professores_regentes = db.relationship('ProfessorTurma', back_populates='turma', cascade='all, delete-orphan')
