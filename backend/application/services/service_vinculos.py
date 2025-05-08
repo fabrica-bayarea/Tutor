1e8f765784605f03b20eacfd084a3cea007e9f87
Victor <victorhenrique.alves.lopes@gmail.com>
2025-05-07 21:12:53 -0300

    Atualizando funções de busca e criação em 'service_vinculos'

--- backend/application/services/service_vinculos.py
+++ backend/application/services/service_vinculos.py
@@ -10,9 +10,9 @@ from application.models import AlunoTurma, ProfessorTurmaMateria, ArquivoTurmaMa
 
 def buscar_vinculos_aluno_turma(aluno_id: uuid.UUID = None, turma_id: uuid.UUID = None) -> list[AlunoTurma]:
     """
-    Busca vínculos entre alunos e turmas baseado em um ou ambos os parâmetros.
+    Busca vínculos entre alunos e turmas.
 
-    Espera receber um desses dois parâmetros:
+    Espera receber um ou ambos esses dois parâmetros:
     - `aluno_id`: uuid.UUID - o ID do aluno
     - `turma_id`: uuid.UUID - o ID da turma
 
@@ -20,15 +20,9 @@ def buscar_vinculos_aluno_turma(aluno_id: uuid.UUID = None, turma_id: uuid.UUID 
     Se nenhum parâmetro for fornecido, retorna None.
     """
     if not aluno_id and not turma_id:
-        return None
+        raise ValueError("É obrigatório fornecer, no mínimo, um ID de aluno ou um ID de turma.")
     
-    query = AlunoTurma.query
-
-    if aluno_id is not None:
-        query = query.filter_by(aluno_id=aluno_id)
-    
-    if turma_id is not None:
-        query = query.filter_by(turma_id=turma_id)
+    query = AlunoTurma.query.filter_by(aluno_id=aluno_id, turma_id=turma_id)
     
     return query.all()
 
@@ -36,12 +30,15 @@ def criar_vinculo_aluno_turma(aluno_id: uuid.UUID, turma_id: uuid.UUID) -> bool:
     """
     Cria um novo vínculo entre um aluno e uma turma.
 
-    Espera receber:
+    Espera receber todos esses dois parâmetros:
     - `aluno_id`: uuid.UUID - o ID do aluno
     - `turma_id`: uuid.UUID - o ID da turma
 
     Retorna True se o vínculo for criado com sucesso, e False se o vínculo já existir.
     """
+    if not aluno_id or not turma_id:
+        raise ValueError("É obrigatório fornecer um ID de aluno e um ID de turma.")
+    
     existe = buscar_vinculo_aluno_turma(aluno_id, turma_id)
     if existe:
         return False
@@ -54,9 +51,9 @@ def criar_vinculo_aluno_turma(aluno_id: uuid.UUID, turma_id: uuid.UUID) -> bool:
 
 def buscar_vinculos_professor_turma_materia(professor_id: uuid.UUID = None, turma_id: uuid.UUID = None, materia_id: uuid.UUID = None) -> list[ProfessorTurmaMateria]:
     """
-    Busca vínculos entre professores, turmas e matérias baseado em um ou todos os parâmetros.
+    Busca vínculos entre professores, turmas e matérias.
 
-    Espera receber um desses três parâmetros:
+    Espera receber um, dois ou todos esses três parâmetros:
     - `professor_id`: uuid.UUID - o ID do professor
     - `turma_id`: uuid.UUID - o ID da turma
     - `materia_id`: uuid.UUID - o ID da matéria
@@ -65,36 +62,75 @@ def buscar_vinculos_professor_turma_materia(professor_id: uuid.UUID = None, turm
     Se nenhum parâmetro for fornecido, retorna None.
     """
     if not professor_id and not turma_id and not materia_id:
-        return None
+        raise ValueError("É obrigatório fornecer, no mínimo, um ID de professor, um ID de turma ou um ID de matéria.")
     
-    query = ProfessorTurmaMateria.query
-
-    if professor_id is not None:
-        query = query.filter_by(professor_id=professor_id)
-    
-    if turma_id is not None:
-        query = query.filter_by(turma_id=turma_id)
-    
-    if materia_id is not None:
-        query = query.filter_by(materia_id=materia_id)
+    query = ProfessorTurmaMateria.query.filter_by(professor_id=professor_id, turma_id=turma_id, materia_id=materia_id)
     
     return query.all()
 
-def criar_vinculo_professor_turma_materia(professor_id: uuid.UUID, turma_id: uuid.UUID, materia_id: uuid.UUID) -> bool:
+def criar_vinculo_professor_turma_materia(professor_id: uuid.UUID, turma_id: uuid.UUID, materia_id: uuid.UUID) -> ProfessorTurmaMateria | None:
     """
     Cria um novo vínculo entre um professor, uma turma e uma matéria.
 
-    Espera receber:
+    Espera receber todos esses três parâmetros:
     - `professor_id`: uuid.UUID - o ID do professor
     - `turma_id`: uuid.UUID - o ID da turma
     - `materia_id`: uuid.UUID - o ID da matéria
 
-    Retorna True se o vínculo for criado com sucesso, e False se o vínculo já existir.
+    Retorna o novo vínculo se ele for criado com sucesso, e None se o vínculo já existir.
     """
+    if not professor_id or not turma_id or not materia_id:
+        raise ValueError("É obrigatório fornecer um ID de professor, um ID de turma e um ID de matéria.")
+    
     existe = buscar_vinculo_professor_turma_materia(professor_id, turma_id, materia_id)
     if existe:
-        return False
+        return None
     
-    db.session.add(ProfessorTurmaMateria(professor_id=professor_id, turma_id=turma_id, materia_id=materia_id))
+    novo_vinculo = ProfessorTurmaMateria(professor_id=professor_id, turma_id=turma_id, materia_id=materia_id)
+    db.session.add(novo_vinculo)
     db.session.commit()
-    return True
+    return novo_vinculo
+
+# -------------------- ARQUIVO <-> TURMA <-> MATÉRIA --------------------
+
+def buscar_vinculos_arquivo_turma_materia(arquivo_id: uuid.UUID = None, turma_id: uuid.UUID = None, materia_id: uuid.UUID = None) -> list[ArquivoTurmaMateria]:
+    """
+    Busca vínculos entre arquivos, turmas e matérias.
+
+    Espera receber um, dois ou todos esses três parâmetros:
+    - `arquivo_id`: uuid.UUID - o ID do arquivo
+    - `turma_id`: uuid.UUID - o ID da turma
+    - `materia_id`: uuid.UUID - o ID da matéria
+
+    Retorna uma lista de vínculos ArquivoTurmaMateria que correspondem aos filtros.
+    Se nenhum parâmetro for fornecido, retorna None.
+    """
+    if not arquivo_id and not turma_id and not materia_id:
+        raise ValueError("É obrigatório fornecer, no mínimo, um ID de arquivo, um ID de turma ou um ID de matéria.")
+    
+    query = ArquivoTurmaMateria.query.filter_by(arquivo_id=arquivo_id, turma_id=turma_id, materia_id=materia_id)
+    
+    return query.all()
+
+def criar_vinculo_arquivo_turma_materia(arquivo_id: uuid.UUID, turma_id: uuid.UUID, materia_id: uuid.UUID) -> ArquivoTurmaMateria | None:
+    """
+    Cria um novo vínculo entre um arquivo, uma turma e uma matéria.
+
+    Espera receber todos esses três parâmetros:
+    - `arquivo_id`: uuid.UUID - o ID do arquivo
+    - `turma_id`: uuid.UUID - o ID da turma
+    - `materia_id`: uuid.UUID - o ID da matéria
+
+    Retorna o vínculo criado se ele for criado com sucesso, e None se o vínculo já existir.
+    """
+    if not arquivo_id or not turma_id or not materia_id:
+        raise ValueError("É obrigatório fornecer um ID de arquivo, um ID de turma e um ID de matéria.")
+    
+    existe = buscar_vinculos_arquivo_turma_materia(arquivo_id, turma_id, materia_id)
+    if existe:
+        return None
+    
+    novo_vinculo = ArquivoTurmaMateria(arquivo_id=arquivo_id, turma_id=turma_id, materia_id=materia_id)
+    db.session.add(novo_vinculo)
+    db.session.commit()
+    return novo_vinculo
