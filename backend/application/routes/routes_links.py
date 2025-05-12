ceecc503e2e94c2a740c19b69d300a56c60e8bd4
Gustavo Drumond <gdrumondduarte@gmail.com>
2025-05-12 19:45:01 -0300

    Finalizando função processar links

--- backend/application/routes/routes_links.py
+++ backend/application/routes/routes_links.py
@@ -1,8 +1,12 @@
 from flask import Blueprint, request, jsonify
-from application.services.service_scraping import *
+from application.services.service_arquivo import *
 from urllib.parse import urlparse
 import time
-
+import json
+from application.services.service_arquivo import *
+from application.services.service_professor import *
+from application.services.service_turma import *
+from application.services.service_materia import *
 
 links_bp = Blueprint('links', __name__)
 
@@ -42,22 +46,61 @@ def scrap_links():
         return jsonify({"error": "URL inválida", "status": "error"}), 400
     
     print(f'\nProcessando {len(valid_urls)} URLs')
-    driver = configure_browser()
     results = []
     
+    matricula_professor = request.json.get('matricula_professor')
+    vinculos = request.json.get('vinculos') # Mesmo recebendo numa estrutura JSON, ele virá como string
+
+
+    if not matricula_professor or not vinculos:
+        return jsonify({"error": "Parâmetros 'matricula_professor' e 'vinculos' são obrigatórios"}), 400
+    
+    # Valida o professor
+    professor_id = buscar_id_professor_por_matricula(matricula_professor)
+    if not professor_id:
+        print(f"Professor com matrícula '{matricula_professor}' não encontrado")
+        return jsonify({"error": f"Professor com matrícula '{matricula_professor}' não encontrado"}), 404
+    
+    # Valida os vínculos
+    vinculos_processados: list[dict[str, uuid.UUID]] = []
+    for vinculo in vinculos:
+        codigo_turma = vinculo.get('codigo_turma')
+        codigo_materia = vinculo.get('codigo_materia')
+
+        if not codigo_turma or not codigo_materia:
+            return jsonify({"error": "Cada vínculo deve conter 'codigo_turma' e 'codigo_materia'"}), 400
+        
+        # Busca o UUID da turma
+        turma_id = buscar_id_turma_por_codigo(codigo_turma)
+        if not turma_id:
+            print(f"Código de turma '{codigo_turma}' não encontrado")
+            return jsonify({"error": f"Código de turma '{codigo_turma}' não encontrado"}), 404
+        
+        # Busca o UUID da matéria
+        materia_id = buscar_id_materia_por_codigo(codigo_materia)
+        if not materia_id:
+            print(f"Código de matéria '{codigo_materia}' não encontrado")
+            return jsonify({"error": f"Código de matéria '{codigo_materia}' não encontrado"}), 404
+        
+        # Adiciona o vínculo processado
+        vinculos_processados.append({"turma_id": turma_id, "materia_id": materia_id})
+    
     try:
+        print('Configurando driver')
+        driver = configure_browser()
+        print('Driver configurado')
         for url in valid_urls:
             print(f'Processando URL: {url}')
-            result = data_extraction(driver, url)
+            result = processar_link(url, driver, professor_id, vinculos_processados)
             results.append(result)
             print(f'URL processada com sucesso: {result}')
             time.sleep(2)
-            
+        driver.quit()
         return jsonify({
             "status": "success",
             'processed_url': len(valid_urls),
             "results": results
-        })
+        }), 201
     except Exception as e:
         return jsonify({
             'status': 'error',
