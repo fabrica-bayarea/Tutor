7d9c12006c40b96b7a706eb966cb277fda870a27
Victor <victorhenrique.alves.lopes@gmail.com>
2025-05-28 23:55:48 -0300

    Corrigindo campo 'data_envio' no modelo de Mensagem

--- backend/application/models/model_mensagem.py
+++ backend/application/models/model_mensagem.py
@@ -1,5 +1,6 @@
 from application.config import db
 import uuid
+from datetime import datetime
 
 class Mensagem(db.Model):
     __tablename__ = 'mensagens'
@@ -8,4 +9,4 @@ class Mensagem(db.Model):
     chat_id = db.Column(db.UUID(as_uuid=True), db.ForeignKey('chats.id'), nullable=False)
     sender_id = db.Column(db.UUID(as_uuid=True), db.ForeignKey('alunos.id'), nullable=False)
     conteudo = db.Column(db.String(3200), nullable=False)
-    data_envio = db.Column(db.DateTime, nullable=False)
+    data_envio = db.Column(db.DateTime, nullable=False, default=datetime.now)
