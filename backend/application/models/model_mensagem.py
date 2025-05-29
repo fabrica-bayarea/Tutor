22813e5608a8c08fb8adfcd9fd65c48cd0b2db53
Victor <victorhenrique.alves.lopes@gmail.com>
2025-05-28 21:30:55 -0300

    Adicionando novos modelos para Chat e Mensagem

---
+++ backend/application/models/model_mensagem.py
@@ -0,0 +1,11 @@
+from application.config import db
+import uuid
+
+class Mensagem(db.Model):
+    __tablename__ = 'mensagens'
+
+    id = db.Column(db.UUID(as_uuid=True, primary_key=True, default=uuid.uuid4))
+    chat_id = db.Column(db.UUID(as_uuid=True), db.ForeignKey('chats.id'), nullable=False)
+    sender_id = db.Column(db.UUID(as_uuid=True), db.ForeignKey('alunos.id'), nullable=False)
+    conteudo = db.Column(db.String(3200), nullable=False)
+    data_envio = db.Column(db.DateTime, nullable=False)
