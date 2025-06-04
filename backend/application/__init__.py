69b9323792b339c9b3346e5892bf69d34f38416e
Victor <victorhenrique.alves.lopes@gmail.com>
2025-06-04 00:19:34 -0300

    Criando um UUID fixo para ser usado em todas as mensagens geradas pela LLM

--- backend/application/__init__.py
+++ backend/application/__init__.py
@@ -3,3 +3,4 @@ from .models import *
 from .routes import *
 from .services import *
 from .utils import *
+from .constants import *
---
+++ backend/application/constants.py
@@ -0,0 +1,4 @@
+import uuid
+
+# UUID fixo para identificar as mensagens geradas pela LLM
+LLM_UUID = uuid.UUID('ae5905d6-cb44-4884-9408-6fcd7cb3b27c')
