7b7b9b2e480faab8f0fe52c4a34d3e1a57d9a31a
Victor <victorhenrique.alves.lopes@gmail.com>
2025-06-13 00:22:45 -0300

    Criando a página de Chat para os alunos (incompleta)

---
+++ frontend/tutor/app/aluno/chat/[chatId]/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import { useState } from 'react';
+
+export default function Chat() {
+    const [chat, setChat] = useState({});
+    return (
+        <div>
+            <h1>Chat</h1>
+        </div>
+    );
+}
