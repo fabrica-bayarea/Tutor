4c9cd20c5bc165070c94f7be03cb60161a0173b3
Victor <victorhenrique.alves.lopes@gmail.com>
2025-06-16 21:13:57 -0300

    Criando novo componente 'CardMateria'

---
+++ frontend/tutor/app/professor/components/CardMateria/CardMateria.tsx
@@ -0,0 +1,41 @@
+import styles from './CardMateria.module.css';
+import { InterfaceMateria, InterfaceTurma } from '../../../types';
+
+export default function CardMateria({
+    materia: {
+        id: materia_id,
+        codigo: materia_codigo,
+        nome: materia_nome,
+    },
+    turma: {
+        id: turma_id,
+        codigo: turma_codigo,
+        semestre: turma_semestre,
+        turno: turma_turno
+    }
+}: {
+    materia: InterfaceMateria,
+    turma: InterfaceTurma
+}) {
+    const materia = {
+        id: materia_id,
+        codigo: materia_codigo,
+        nome: materia_nome,
+    }
+    
+    const turma = {
+        id: turma_id,
+        codigo: turma_codigo,
+        semestre: turma_semestre,
+        turno: turma_turno
+    }
+    
+    return (
+        <div className={styles.cardMateria}>
+            <div className={styles.cardMateriaStateLayer}>
+                <h3 className={styles.cardMateriaNome}>{materia.nome}</h3>
+                <h4 className={styles.cardMateriaTurmaCodigo}>{turma.codigo} - {materia.codigo}</h4>
+            </div>
+        </div>
+    )
+}
