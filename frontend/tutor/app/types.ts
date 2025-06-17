2a983675f3fb5bc8cb15084a18f9c3f3632ae59c
Victor <victorhenrique.alves.lopes@gmail.com>
2025-06-16 21:07:37 -0300

    Criando algumas interfaces globais de forma centralizada

---
+++ frontend/tutor/app/types.ts
@@ -0,0 +1,48 @@
+export interface InterfaceAluno {
+    id: string;
+    nome: string;
+    email: string;
+    telefone: string;
+    data_nascimento: Date;
+}
+
+export interface InterfaceProfessor {
+    id: string;
+    nome: string;
+    email: string;
+    telefone: string;
+    data_nascimento: Date;
+}
+
+export interface InterfaceChat {
+    id: string;
+    aluno_id: string;
+    nome: string;
+    data_ultima_interacao: Date;
+}
+
+export interface InterfaceMensagem {
+    id: string;
+    chat_id: string;
+    sender_id: string;
+    conteudo: string;
+    data_envio: Date;
+}
+
+export interface InterfaceTurma {
+    id: string;
+    codigo: string;
+    semestre: string;
+    turno: string;
+}
+
+export interface InterfaceMateria {
+    id: string;
+    codigo: string;
+    nome: string;
+}
+
+export interface InterfaceTurmaMateria {
+    turma_id: string;
+    materia_id: string;
+}
