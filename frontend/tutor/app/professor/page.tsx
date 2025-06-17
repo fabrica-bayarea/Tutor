edb0796f1e5b35005ecdf230e5d0069f7a78a49d
Victor <victorhenrique.alves.lopes@gmail.com>
2025-06-16 21:26:03 -0300

    Implementando interface de Professor

--- frontend/tutor/app/professor/page.tsx
+++ frontend/tutor/app/professor/page.tsx
@@ -4,9 +4,16 @@ import { useEffect, useState } from 'react';
 import styles from './page.module.css';
 
 import Aside from './components/Aside/Aside';
+import { InterfaceProfessor } from '../types';
 
 export default function Home() {
-    const [professor, setProfessor] = useState({ id: '550e8400-e29b-41d4-a716-446655440000', matricula: '1', nome: 'Regiano', email: 'regiano@gmail.com' }); // ...senha, cpf, data_nascimento
+    const [professor, setProfessor] = useState<InterfaceProfessor>({
+        id: '550e8400-e29b-41d4-a716-446655440000',
+        matricula: '1',
+        nome: 'Regiano',
+        email: 'regiano@gmail.com',
+        cpf: '12345678900', // ...senha, data_nascimento
+    });
     return (
         <div className={styles.pageContainer}>
             <Aside selected='home' />
