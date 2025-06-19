4ac96029fc01a85957e08d33a54d8aa46c077c3b
Victor <victorhenrique.alves.lopes@gmail.com>
2025-06-19 00:36:36 -0300

    Movendo tudo do frontend direto para a pasta 'frontend/', para evitar a redundância de 'frontend/tutor/'

--- frontend/app/aluno/chat/[chatId]/page.tsx
+++ frontend/app/aluno/chat/[chatId]/page.tsx
@@ -4,6 +4,8 @@ import { useState } from 'react';
 
 export default function Chat() {
     const [chat, setChat] = useState({});
+    const [messages, setMessages] = useState([]);
+    
     return (
         <div>
             <h1>Chat</h1>
