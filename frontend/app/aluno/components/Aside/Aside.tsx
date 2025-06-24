c7480bf92fc6c0e2348cc5c37ccd0e9793dfdc59
Victor <victorhenrique.alves.lopes@gmail.com>
2025-06-24 16:37:01 -0300

    Atualizando o Aside dos alunos pra se atualizar dinamicamente; Implementando o uso de interfaces no Aside

--- frontend/app/aluno/components/Aside/Aside.tsx
+++ frontend/app/aluno/components/Aside/Aside.tsx
@@ -1,20 +1,36 @@
-import { useState } from 'react';
+'use client';
+
+import { useEffect, useState } from 'react';
+import { usePathname, useParams } from 'next/navigation';
 import styles from './Aside.module.css';
 import { Menu, SquarePen } from 'lucide-react';
 import AsideMainButton from '../../../components/AsideMainButton/AsideMainButton';
 import ChatListItemButton from '../ChatListItemButton/ChatListItemButton';
-type AsideAlunoProps = {
-    selected: 'home';
-};
+import { InterfaceChat } from '../../../types';
 
-export default function Aside({ selected }: AsideAlunoProps) {
-    const [chats, setChats] = useState([
+export default function Aside() {
+    const pathname = usePathname();
+    const params = useParams();
+    
+    const getSelected = () => {
+        if (pathname.endsWith('/aluno')) return 'home';
+        return null;
+    };
+    const selectedSection = getSelected();
+    
+    const [chats, setChats] = useState<InterfaceChat[]>([
         {
             id: 'af734cdf-1106-49ec-8b26-be87598c992c',
             aluno_id: 'cde982bc-2c4b-43a0-8439-eba9d2149306',
-            name: 'Chat de teste',
+            nome: 'Chat de teste',
         },
     ]);
+    
+    const selectedChatId = params.chatId;
+    useEffect(() => {
+        
+    }, []);
+    
     return (
         <div className={styles.asideContainer}>
             <div className={styles.asideHeader}>
@@ -22,12 +38,14 @@ export default function Aside({ selected }: AsideAlunoProps) {
                 <Menu size={24} />
             </div>
             <div className={styles.asideMainButtons}>
-                <AsideMainButton
-                    icon={<SquarePen />}
-                    label="Novo chat"
-                    isCollapsed={false}
-                    isSelected={selected === 'home'}
-                />
+                <a href="/aluno">
+                    <AsideMainButton
+                        icon={<SquarePen />}
+                        label="Novo chat"
+                        isCollapsed={false}
+                        isSelected={selectedSection === 'home'}
+                    />
+                </a>
             </div>
             <div className={styles.asideChatList}>
                 {chats.map((chat) => (
@@ -35,8 +53,8 @@ export default function Aside({ selected }: AsideAlunoProps) {
                         key={chat.id}
                         id={chat.id}
                         aluno_id={chat.aluno_id}
-                        name={chat.name}
-                        isSelected={chat.id === selected}
+                        name={chat.nome}
+                        isSelected={chat.id === selectedChatId}
                     />
                 ))}
             </div>
