3a9799e9b67c686ae8afae71fde9cb6dc4afc2a5
Victor <victorhenrique.alves.lopes@gmail.com>
2025-06-26 23:55:02 -0300

    Criando funções para criar e buscar professores e alunos, e aprimorando a função para logar os mesmos

--- backend/application/services/service_aluno.py
+++ backend/application/services/service_aluno.py
@@ -2,7 +2,7 @@ import uuid
 from application.models import Aluno
 from application.config import db
 
-def criar_aluno(matricula: str, nome: str, email: str, cpf: str, data_nascimento: str) -> dict[str, str] | None:
+def criar_aluno(matricula: str, nome: str, email: str, senha: str, cpf: str, data_nascimento: str) -> dict[str, str] | None:
     """
     Função atômica, responsável por criar um aluno no PostgreSQL.
 
@@ -10,15 +10,17 @@ def criar_aluno(matricula: str, nome: str, email: str, cpf: str, data_nascimento
     - `matricula`: str - o número de matrícula do aluno
     - `nome`: str - o nome do aluno
     - `email`: str - o email do aluno
+    - `senha`: str - a senha do aluno
     - `cpf`: str - o cpf do aluno
     - `data_nascimento`: str - a data de nascimento do aluno
 
-    Retorna o aluno criado.
+    Retorna um dicionário com os dados do aluno criado.
     """
     aluno = Aluno(
         matricula=matricula,
         nome=nome,
         email=email,
+        senha=senha,
         cpf=cpf,
         data_nascimento=data_nascimento
     )
@@ -26,16 +28,38 @@ def criar_aluno(matricula: str, nome: str, email: str, cpf: str, data_nascimento
     db.session.commit()
     return aluno.to_dict()
 
-def buscar_aluno_por_id(aluno_id: uuid.UUID) -> dict[str, str] | None:
+def buscar_aluno(aluno_id: uuid.UUID = None, matricula: str = None, nome: str = None, email: str = None, cpf: str = None, data_nascimento: str = None) -> dict[str, str] | None:
     """
-    Busca um aluno no banco de dados usando o ID fornecido.
+    Busca um aluno no banco de dados usando um ou mais filtros.
 
-    Espera receber:
+    Espera receber um ou mais dos seguintes parâmetros:
     - `aluno_id`: uuid.UUID - o ID do aluno
+    - `matricula`: str - o número de matrícula do aluno
+    - `nome`: str - o nome do aluno
+    - `email`: str - o email do aluno
+    - `cpf`: str - o cpf do aluno
+    - `data_nascimento`: str - a data de nascimento do aluno
 
-    Retorna o aluno se ele existir, e None caso contrário.
+    Retorna um dicionário com os dados do aluno se ele existir, e None caso contrário.
     """
-    aluno = Aluno.query.get(aluno_id)
+    if aluno_id is not None:
+        aluno = Aluno.query.filter_by(aluno_id=aluno_id).first()
+    
+    if matricula is not None:
+        aluno = Aluno.query.filter_by(matricula=matricula).first()
+    
+    if nome is not None:
+        aluno = Aluno.query.filter_by(nome=nome).first()
+    
+    if email is not None:
+        aluno = Aluno.query.filter_by(email=email).first()
+    
+    if cpf is not None:
+        aluno = Aluno.query.filter_by(cpf=cpf).first()
+    
+    if data_nascimento is not None:
+        aluno = Aluno.query.filter_by(data_nascimento=data_nascimento).first()
+    
     return aluno.to_dict() if aluno else None
 
 def logar_aluno(matricula: str, senha: str) -> dict[str, str] | None:
@@ -46,7 +70,14 @@ def logar_aluno(matricula: str, senha: str) -> dict[str, str] | None:
     - `matricula`: str - o número de matrícula do aluno
     - `senha`: str - a senha do aluno
 
-    Retorna os dados do aluno se o login for válido, e None caso contrário.
+    1. Busca um aluno no banco de dados usando o número de matrícula fornecido
+    2. Verifica se o aluno existe e se a senha fornecida é igual à senha do aluno
+    
+    Retorna um dicionário com os dados do aluno se o login for válido, e None caso contrário.
     """
-    aluno = Aluno.query.filter_by(matricula=matricula, senha=senha).first()
-    return aluno.to_dict() if aluno else None
+    aluno = Aluno.query.filter_by(matricula=matricula).first()
+    
+    if aluno and aluno.senha == senha:
+        return aluno.to_dict()
+    
+    return None
--- backend/application/services/service_professor.py
+++ backend/application/services/service_professor.py
@@ -1,17 +1,66 @@
 import uuid
 from application.models import Professor
+from application.config import db
 
-def buscar_id_professor_por_matricula(matricula: str) -> uuid.UUID | None:
+def criar_professor(matricula: str, nome: str, email: str, senha: str, cpf: str, data_nascimento: str) -> dict[str, str] | None:
     """
-    Busca um professor no banco de dados usando a matrícula fornecida.
+    Função atômica, responsável por criar um professor no PostgreSQL.
 
     Espera receber:
     - `matricula`: str - o número de matrícula do professor
-    
-    Retorna o UUID do professor se ele existir, e None caso contrário.
+    - `nome`: str - o nome do professor
+    - `email`: str - o email do professor
+    - `senha`: str - a senha do professor
+    - `cpf`: str - o cpf do professor
+    - `data_nascimento`: str - a data de nascimento do professor
+
+    Retorna o professor criado.
     """
-    professor = Professor.query.filter_by(matricula=matricula).first()
-    return professor.id if professor else None
+    professor = Professor(
+        matricula=matricula,
+        nome=nome,
+        email=email,
+        senha=senha,
+        cpf=cpf,
+        data_nascimento=data_nascimento
+    )
+    db.session.add(professor)
+    db.session.commit()
+    return professor.to_dict()
+
+def buscar_professor(professor_id: uuid.UUID = None, matricula: str = None, nome: str = None, email: str = None, cpf: str = None, data_nascimento: str = None) -> dict[str, str] | None:
+    """
+    Busca um professor no banco de dados usando um ou mais filtros.
+
+    Espera receber um ou mais dos seguintes parâmetros:
+    - `professor_id`: uuid.UUID - o ID do professor
+    - `matricula`: str - o número de matrícula do professor
+    - `nome`: str - o nome do professor
+    - `email`: str - o email do professor
+    - `cpf`: str - o cpf do professor
+    - `data_nascimento`: str - a data de nascimento do professor
+
+    Retorna o professor se ele existir, e None caso contrário.
+    """
+    if professor_id is not None:
+        professor = Professor.query.filter_by(professor_id=professor_id).first()
+    
+    if matricula is not None:
+        professor = Professor.query.filter_by(matricula=matricula).first()
+    
+    if nome is not None:
+        professor = Professor.query.filter_by(nome=nome).first()
+    
+    if email is not None:
+        professor = Professor.query.filter_by(email=email).first()
+    
+    if cpf is not None:
+        professor = Professor.query.filter_by(cpf=cpf).first()
+    
+    if data_nascimento is not None:
+        professor = Professor.query.filter_by(data_nascimento=data_nascimento).first()
+    
+    return professor.to_dict() if professor else None
 
 def logar_professor(matricula: str, senha: str) -> dict[str, str] | None:
     """
@@ -21,7 +70,14 @@ def logar_professor(matricula: str, senha: str) -> dict[str, str] | None:
     - `matricula`: str - o número de matrícula do professor
     - `senha`: str - a senha do professor
 
+    1. Busca um professor no banco de dados usando o número de matrícula fornecido
+    2. Verifica se o professor existe e se a senha fornecida é igual à senha do professor
+    
     Retorna os dados do professor se o login for válido, e None caso contrário.
     """
-    professor = Professor.query.filter_by(matricula=matricula, senha=senha).first()
-    return professor.to_dict() if professor else None
+    professor = Professor.query.filter_by(matricula=matricula).first()
+    
+    if professor and professor.senha == senha:
+        return professor.to_dict()
+    
+    return None
