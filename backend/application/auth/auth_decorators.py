85e6aeb406cfe7ca83caa4c297e3d85e8889eb1f
Victor <victorhenrique.alves.lopes@gmail.com>
2025-06-29 00:12:07 -0300

    Criando decorador para restringir o uso de determinados endpoints apenas para professores

--- backend/application/auth/auth_decorators.py
+++ backend/application/auth/auth_decorators.py
@@ -3,6 +3,9 @@ from flask import request, jsonify, g
 from .jwt_handler import validar_token
 
 def token_obrigatorio(f):
+    """
+    Decorador personalizado que verifica se um token JWT válido foi enviado numa requisição.
+    """
     @wraps(f)
     def wrapper(*args, **kwargs):
         token = request.headers.get("Authorization", "").replace("Bearer ", "")
@@ -14,5 +17,17 @@ def token_obrigatorio(f):
             return jsonify({"error": "Token inválido ou expirado"}), 401
         
         g.usuario_id = payload.get("user_id")
+        g.usuario_role = payload.get("role")
+        return f(*args, **kwargs)
+    return wrapper
+
+def apenas_professores(f):
+    """
+    Decorador personalizado que restringe o acesso da rota apenas para usuários com papel de professor.
+    """
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        if not hasattr(g, "usuario_role") or g.usuario_role != "professor":
+            return jsonify({"error": "Acesso negado"}), 403
         return f(*args, **kwargs)
     return wrapper
