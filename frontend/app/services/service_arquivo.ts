97f201eb7d959b0a66fadaf8196b3a4e04e891e9
Victor <victorhenrique.alves.lopes@gmail.com>
2025-07-01 16:30:14 -0300

    Reestruturando 'service_arquivo.ts'

--- frontend/app/services/service_arquivo.ts
+++ frontend/app/services/service_arquivo.ts
@@ -1,33 +1,61 @@
 import api from "./api";
+import { InterfaceArquivo, InterfaceTurmaMateria } from "../types";
+
+interface InterfaceUploadResponse {
+    message: string;
+    results: {
+        filename: string;
+        status: number;
+        data: InterfaceArquivo;
+    }[];
+}
 
 const arquivos_url = "arquivos";
 
-async function postUpload(files: File[], matricula_professor: string, vinculos: any[]) {
+export async function uploadArquivos(arquivos: File[], vinculos: InterfaceTurmaMateria[]) {
     try {
         const formData = new FormData();
         
-        files.forEach((file, index) => {
-            formData.append('arquivos', file);
+        arquivos.forEach((arquivo) => {
+            formData.append('arquivos', arquivo);
         });
-
-        formData.append('matricula_professor', matricula_professor);
 
         formData.append('vinculos', JSON.stringify(vinculos));
 
-        const response = await api.post(`/${arquivos_url}/upload`, formData, {
-            headers: {"Content-Type": "multipart/form-data"}
-        });
-        return response.data;
+        const response: InterfaceUploadResponse = await api.post(`/${arquivos_url}/upload/arquivos`, formData);
+        console.log(response)
+
+        return response;
     } catch (error) {
-        console.error("Erro ao processar o arquivo:", error);
+        console.error("Erro ao processar o(s) arquivo(s):", error);
         throw error;
     }
 }
 
-async function getArquivos(id_professor){
-    pass
+export async function uploadLinks(links: string[], vinculos: InterfaceTurmaMateria[]) {
+    try {
+        const response: InterfaceUploadResponse = await api.post(`/${arquivos_url}/upload/links`, { links, vinculos });
+        console.log(response)
+
+        return response;
+    } catch (error) {
+        console.error("Erro ao processar o(s) link(s):", error);
+        throw error;
+    }
 }
 
+export async function uploadTextos(textos: string[], vinculos: InterfaceTurmaMateria[]) {
+    try {
+        const response: InterfaceUploadResponse = await api.post(`/${arquivos_url}/upload/textos`, { textos, vinculos });
+        console.log(response)
 
+        return response;
+    } catch (error) {
+        console.error("Erro ao processar o(s) texto(s):", error);
+        throw error;
+    }
+}
 
-export {postUpload, getArquivos};
+// export async function getArquivos(id_professor){
+//     pass
+// }
