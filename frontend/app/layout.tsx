cb7f33f7fe7db604d333f34af13dbb2d033f2b82
Victor <victorhenrique.alves.lopes@gmail.com>
2025-07-03 17:41:52 -0300

    Alterando o layout da raiz do front-end

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,37 +1,23 @@
-// Importa o tipo Metadata usado para definir metadados da página
-import type { Metadata } from "next";
-
-// Importa estilos globais da aplicação
-import "./globals.css";
 import React from "react";
-import MainNavigator from './components/mainNavigator';
+import type { Metadata } from "next";
+import "./globals.css";
 import { ModalProvider } from "./contexts/contextModal";
 
-/**
- * Metadados básicos da aplicação definidos para o HTML <head>.
- * O Next.js usará esse objeto para gerar as tags correspondentes automaticamente.
- */
 export const metadata: Metadata = {
-  title: "Tutor",
+    title: "Tutor",
 };
-/**
- * RootLayout define o layout base da aplicação Next.js.
- * Este componente é o contêiner principal que envolve todas as páginas.
- */
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="pt-br"> 
-      <body>
-        <ModalProvider>
-          {/* <MainNavigator /> */}
-          <main>{children}</main>
-        </ModalProvider>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="pt-br">
+            <body>
+                <ModalProvider>
+                    <main>{children}</main>
+                </ModalProvider>
+            </body>
+        </html>
+    );
 }
-
