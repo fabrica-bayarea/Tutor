ec74a216159b0f3ff0ccc9e7652ac6124dbd8cb9
kaynan <kaynanm61@gmail.com>
2025-07-03 12:01:13 -0300

    Estrutura inicial para Websockets

--- backend/application/socket/events.py
+++ backend/application/socket/events.py
@@ -6,3 +6,11 @@ socketio = SocketIO(cors_allowed_origins="*", async_mode="eventlet")
 @socketio.on("connect")
 def on_connect():
     handle_connect()
+
+@socketio.on('mensagem_inicial')
+def on_mensagem_inicial():
+    handle_mensagem_inicial()
+
+@socketio.on('nova_mensagem')
+def on_nova_mensagem():
+    handle_nova_mensagem()
