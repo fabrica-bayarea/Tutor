a7b433f9a55b34392d4b38f74ae67c12fcce3f67
Victor <victorhenrique.alves.lopes@gmail.com>
2025-07-03 17:43:46 -0300

    Alterando o comportamento do front-end ao estar desautenticado

--- frontend/app/services/api.ts
+++ frontend/app/services/api.ts
@@ -19,8 +19,11 @@ api.interceptors.response.use(
     (response) => response,
     (error) => {
         if (error.response?.status === 401) {
-            console.warn("Token inválido ou expirado. Redirecionando para login...");
-            window.location.href = "/login";
+            const currentPath = window.location.pathname;
+            if (!currentPath.startsWith('/login')) {
+                const returnTo = encodeURIComponent(currentPath);
+                window.location.href = `/login?returnTo=${returnTo}`;
+            }
         }
         return Promise.reject(error);
     }
