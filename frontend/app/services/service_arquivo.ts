ddee0f0df9d1f726c8a0fb16104003e8089a2f54
Victor <victorhenrique.alves.lopes@gmail.com>
2025-07-04 16:57:11 -0300

    Modificando interfaces de resposta; Alterando parâmetro da função 'uploadLinks()' de 'links' para 'urls' para não conflitar com o endpoint no back-end

--- frontend/app/services/service_arquivo.ts
+++ frontend/app/services/service_arquivo.ts
@@ -22,22 +22,20 @@ export async function uploadArquivos(arquivos: File[], vinculos: InterfaceTurmaM
 
         formData.append('vinculos', JSON.stringify(vinculos));
 
-        const response: InterfaceUploadResponse = await api.post(`/${arquivos_url}/upload/arquivos`, formData);
-        console.log(response)
+        const response: { data: InterfaceUploadResponse } = await api.post(`/${arquivos_url}/upload/arquivos`, formData);
 
-        return response;
+        return response.data;
     } catch (error) {
         console.error("Erro ao processar o(s) arquivo(s):", error);
         throw error;
     }
 }
 
-export async function uploadLinks(links: string[], vinculos: InterfaceTurmaMateria[]) {
+export async function uploadLinks(urls: string[], vinculos: InterfaceTurmaMateria[]) {
     try {
-        const response: InterfaceUploadResponse = await api.post(`/${arquivos_url}/upload/links`, { links, vinculos });
-        console.log(response)
+        const response: { data: InterfaceUploadResponse } = await api.post(`/${arquivos_url}/upload/links`, { urls, vinculos });
 
-        return response;
+        return response.data;
     } catch (error) {
         console.error("Erro ao processar o(s) link(s):", error);
         throw error;
@@ -46,10 +44,9 @@ export async function uploadLinks(links: string[], vinculos: InterfaceTurmaMater
 
 export async function uploadTextos(textos: string[], vinculos: InterfaceTurmaMateria[]) {
     try {
-        const response: InterfaceUploadResponse = await api.post(`/${arquivos_url}/upload/textos`, { textos, vinculos });
-        console.log(response)
+        const response: { data: InterfaceUploadResponse } = await api.post(`/${arquivos_url}/upload/textos`, { textos, vinculos });
 
-        return response;
+        return response.data;
     } catch (error) {
         console.error("Erro ao processar o(s) texto(s):", error);
         throw error;
