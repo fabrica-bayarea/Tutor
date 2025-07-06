bfbd45e4c7899368de750523d2a7ba676343e304
Victor <victorhenrique.alves.lopes@gmail.com>
2025-07-06 14:49:48 -0300

    Instanciando a coleção do banco vetorial de forma centralizada

--- backend/application/config/vector_database.py
+++ backend/application/config/vector_database.py
@@ -7,3 +7,11 @@ CHROMADB_PATH = os.path.join(BASE_DIR, "data/chromadb")
 os.makedirs(CHROMADB_PATH, exist_ok=True)
 
 chroma_client = PersistentClient(path=CHROMADB_PATH)
+collection = chroma_client.get_or_create_collection(name="documentos")
+"""
+Coleção com nome "documentos", para realizar todas as operações CRUD envolvendo vetores de documentos, como por exemplo:
+- Criar novos vetores de documentos
+- Fazer buscas semânticas de documentos
+- Atualizar vetores de documentos
+- Excluir vetores de documentos
+"""
