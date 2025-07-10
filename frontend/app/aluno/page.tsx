e3f4ba3d48a001a7d48c7cb525708e05086ab2cf
Victor <victorhenrique.alves.lopes@gmail.com>
2025-07-10 00:58:14 -0300

    Inserindo listeners para eventos emitidos pelo back-end

--- frontend/app/aluno/page.tsx
+++ frontend/app/aluno/page.tsx
@@ -1,30 +1,55 @@
-'use client';
+"use client";
 
-import { useEffect, useState } from 'react';
-import styles from './page.module.css';
-import MessageForm from './components/MessageForm/MessageForm';
-
-import { InterfaceAluno } from '../types';
+import { useEffect, useState } from "react"
+import MessageForm from "./components/MessageForm/MessageForm"
+import styles from "./page.module.css"
+import socket from "@/libs/socket"
+import { useRouter } from "next/navigation"
+import { InterfaceAluno, InterfaceChat } from "../types"
 
 export default function Home() {
-    const [aluno, setAluno] = useState<InterfaceAluno>({ id: 'cde982bc-2c4b-43a0-8439-eba9d2149306', matricula: '2314290141', nome: 'Victor', email: 'victor@gmail.com', cpf: '12345678901', data_nascimento: new Date('2000-01-01') }); // ...senha, cpf, data_nascimento
-    
+    const router = useRouter()
+    const [aluno, setAluno] = useState<InterfaceAluno | null>(null)
+
     useEffect(() => {
-        
-    }, []);
-    
-    function sendMessage(message: string) {
-        console.log(message);
+        const alunoData = localStorage.getItem("aluno")
+        if (alunoData) {
+            try {
+                setAluno(JSON.parse(alunoData))
+            } catch (err) {
+                console.error("Erro ao parsear aluno:", err)
+            }
+        }
+    }, [])
+
+    // Listeners para eventos emitidos pelo back-end
+    useEffect(() => {
+        socket.on("novo_chat", (chat: InterfaceChat) => {
+            console.log(`Novo chat recebido:\n${JSON.stringify(chat)}`)
+            console.log(`Redirecionando para '/aluno/chat/${chat.id}'`)
+            router.push(`/aluno/chat/${chat.id}`)
+            //window.location.href = `/aluno/chat/${chat.id}`
+        })
+    }, [])
+
+    const handleEnviarMensagemInicial = (msg: string) => {
+        if (aluno) {
+            console.log(`Enviando mensagem inicial: ${msg}`)
+            socket.emit("mensagem_inicial", {
+                aluno_id: aluno.id,
+                mensagem: msg,
+            })
+        }
     }
-    
+
     return (
         <div className={styles.midColumn}>
             <div className={styles.headerContainer}>
-                <h1>Olá, {aluno.nome}!</h1>
+                <h1>Olá, {aluno?.nome}!</h1>
                 <h2>Como posso ajudar hoje?</h2>
             </div>
             <div className={styles.bottomContainer}>
-                <MessageForm onSendMessage={sendMessage} />
+                <MessageForm onSendMessage={handleEnviarMensagemInicial} />
                 <span>A inteligência artificial pode cometer erros. Considere checar informações importantes.</span>
             </div>
         </div>
