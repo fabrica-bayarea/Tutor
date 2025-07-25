f5c000536a5b0941e41eed37dc1e5c27f37a3236
Victor <victorhenrique.alves.lopes@gmail.com>
2025-07-25 16:00:17 -0300

    Criando nova constante com classificação de tipos de arquivo

--- frontend/constants.ts
+++ frontend/constants.ts
@@ -1,1 +1,9 @@
 export const LLM_UUID = "ae5905d6-cb44-4884-9408-6fcd7cb3b27c"
+
+export const TIPOS_ARQUIVO = {
+    "documento": [".pdf", ".docx", ".pptx", ".xlsx", ".csv", ".html", ".xhtml", ".md", ".markdown"],
+    "video": [".mp4", ".mov", ".mkv", ".avi"],
+    "audio": [".mp3", ".wav", ".m4a", ".flac", ".ogg"],
+    //"link": ["http", "https"],
+    "texto": ".txt"
+}
