65d5b343b66f843baf23c4b1f7e1a77834b58b53
Victor <victorhenrique.alves.lopes@gmail.com>
2025-08-02 11:07:28 -0300

    Deletando a pasta 'mistral/', já depreciada há um tempo

--- backend/application/mistral/core.py
+++
@@ -1,8 +0,0 @@
-import os
-from .rag_engine import RAGEngine
-from .llm_load import LLMWrapper
-from .rag_pipeline import RAGPipeline
-
-rag = RAGEngine(collection_name="documentos")
-llm = LLMWrapper(model_name="mistral")
-pipeline = RAGPipeline(rag=rag, llm=llm)
