ad9670f26f2b3a725e6ff6f132fd3049c2af4dbf
Luiz Felipe <luizfsilvano@gmail.com>
2025-10-08 13:29:17 -0300

    chore(docker): Adiciona entrypoint script para configuração em runtime

---
+++ frontend/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+NEXT_STATIC_DIR=$(find /app/.next -type d -name "static")
+
+API_URL=${NEXT_PUBLIC_API_URL_RUNTIME:-http://localhost:5000}
+
+echo "Configurando a URL da API para: $API_URL"
+
+grep -rl '__API_URL_PLACEHOLDER__' $NEXT_STATIC_DIR | xargs sed -i "s|__API_URL_PLACEHOLDER__|$API_URL|g"
+
+exec "$@"
