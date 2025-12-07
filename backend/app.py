bbd16bf419f741d21927e8b6f2b704c7155b971c
Ivin <vaz.ivin@gmail.com>
2025-12-07 20:23:23 -0300

    Adicionando correções no app.py

--- backend/app.py
+++ backend/app.py
@@ -3,7 +3,7 @@ from flask_migrate import Migrate
 from flask_cors import CORS
 from application.config.config import Config
 from application.config.database import init_db, db
-from application.routes import arquivos_bp, professores_bp, alunos_bp, turmas_bp, materias_bp, chats_bp, mensagens_bp, vinculos_bp
+from application.routes import arquivos_bp, usuarios_bp, turmas_bp, materias_bp, chats_bp, mensagens_bp, vinculos_bp
 from application.socket import socketio
 
 app = Flask(__name__)
@@ -32,8 +32,7 @@ init_db(app)
 migrate = Migrate(app, db)
 
 app.register_blueprint(arquivos_bp, url_prefix="/arquivos")
-app.register_blueprint(professores_bp, url_prefix="/professores")
-app.register_blueprint(alunos_bp, url_prefix="/alunos")
+app.register_blueprint(usuarios_bp, url_prefix="/usuario")
 app.register_blueprint(turmas_bp, url_prefix="/turmas")
 app.register_blueprint(materias_bp, url_prefix="/materias")
 app.register_blueprint(chats_bp, url_prefix="/chats")
