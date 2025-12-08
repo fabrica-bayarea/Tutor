a1e9a657ea54e28f48534d6e1878499e351aef5b
Ivin <vaz.ivin@gmail.com>
2025-12-08 00:29:34 -0300

    Teoricamente terminando a integração do novo login+redirecionamento+google+alterações no banco

--- backend/application/routes/route_usuarios.py
+++ backend/application/routes/route_usuarios.py
@@ -1,7 +1,10 @@
 """
 Rotas para lidar com alunos.
 """
+import os
 from flask import Blueprint, request, jsonify
+from google.oauth2 import id_token
+from google.auth.transport import requests as grequests
 from application.auth.jwt_handler import gerar_token
 from application.auth.auth_decorators import apenas_admins
 from application.services.service_usuario import criar_aluno, buscar_aluno, logar_aluno, alterar_aluno
@@ -130,3 +133,59 @@ def elegerUsuario:
 
     aluno = alterar_aluno(matricula,role)
     return jsonify(aluno), 200        
+
+@usuarios_bp.route('/buscar',methods['GET'])
+def buscarUsuario:
+
+    matricula = request.json.get('matricula')
+    nome: request.json.get('nome')
+    email: request.json.get('email')
+    role = request.json.get('role')
+
+    if not matricula and not role and not nome and not email:
+        return jsonify({"error": "Parâmetros de busca são obrigatórios"}), 422
+    
+    aluno = buscar_aluno(matricula=matricula,nome=nome,email=email,role=role)
+
+    if not aluno:
+        return jsonify({"error": "Usuário não encontrado"}), 404
+
+    return jsonify(aluno), 200
+
+@usuarios_bp.route('/login/google', methods=['POST'])
+def login_google():
+    data = request.json
+    token = data.get('token')
+    
+    try:
+        idinfo = id_token.verify_oauth2_token(token, grequests.Request(), os.getenv("GOOGLE_CLIENT_ID"))
+
+        email = idinfo['email']
+        nome = idinfo['name']
+        sub = idinfo['sub']
+
+        aluno = Usuario.query.filter_by(email=email).first()
+
+        if not aluno:
+            matricula = f"G{sub}"
+            senha = secrets.token_urlsafe(8)
+
+            aluno = Usuario(
+                matricula=matricula,
+                nome=nome,
+                email=email,
+                senha=senha,
+                role=RoleEnum.ALUNO
+            )
+            db.session.add(aluno)
+            db.session.commit()
+
+        token_jwt = gerar_token(aluno.id, aluno.role.value)
+
+        return jsonify({
+            "aluno": aluno.to_dict(),
+            "token": token_jwt
+        }), 200
+
+    except ValueError:
+        return jsonify({"error": "Token inválido"}), 401
