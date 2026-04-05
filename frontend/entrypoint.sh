9488c8320f405ff784e303e3558fba9525cc34ff
viniciusvon.contato <viniciusvon.contato@gmail.com>
2026-04-05 20:53:48 -0300

    fix: Não executar o xargs sem nenhum arquivo

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -6,6 +6,6 @@ API_URL=${NEXT_PUBLIC_API_URL_RUNTIME:-http://localhost:5000}
 
 echo "Configurando a URL da API para: $API_URL"
 
-grep -rl '__API_URL_PLACEHOLDER__' $NEXT_STATIC_DIR | xargs sed -i "s|__API_URL_PLACEHOLDER__|$API_URL|g"
+grep -rl '__API_URL_PLACEHOLDER__' $NEXT_STATIC_DIR | xargs -r sed -i "s|__API_URL_PLACEHOLDER__|$API_URL|g"
 
 exec "$@"
