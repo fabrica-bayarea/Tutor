57900c106842a69addafc49f9e558acd981d871f
Ivin Vaz Braga Santos <133714762+ivinvaz@users.noreply.github.com>
2026-04-07 14:09:42 -0300

    Adicionando o Componente de textarea do chat

---
+++ frontend/app/(pages)/chat/components/TextAreaChat/TextAreaChat.tsx
@@ -0,0 +1,46 @@
+import { Plus, SendHorizontal } from "lucide-react";
+import styles from "./TextAreaChat.module.css";
+
+interface TextAreaChatProps {
+  value: string;
+  onChange: (val: string) => void;
+  onSend: () => void;
+  onPlusClick?: () => void;
+}
+
+export default function TextAreaChat({
+  value,
+  onChange,
+  onSend,
+  onPlusClick,
+}: TextAreaChatProps) {
+  return (
+    <section className={styles.conteinerTextarea}>
+      <section className={styles.textareaMobile}>
+        <article className={styles.articlePlus} onClick={onPlusClick}>
+          <Plus size={24} />
+        </article>
+        <article className={styles.articleSend}>
+          <textarea
+            placeholder="Envie aqui sua dúvida"
+            value={value}
+            onChange={(e) => onChange(e.target.value)}
+          />
+          <SendHorizontal size={24} onClick={onSend} />
+        </article>
+      </section>
+
+      <section className={styles.textareaDeskTop}>
+        <textarea
+          placeholder="Envie aqui sua dúvida"
+          value={value}
+          onChange={(e) => onChange(e.target.value)}
+        />
+        <article>
+          <Plus size={24} onClick={onPlusClick} />
+          <SendHorizontal size={24} onClick={onSend} />
+        </article>
+      </section>
+    </section>
+  );
+}
