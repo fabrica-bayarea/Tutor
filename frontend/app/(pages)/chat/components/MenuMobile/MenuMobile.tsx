db48003bdd2f7387ae31761386861aba7dc2c6e4
Ivin Vaz Braga Santos <133714762+ivinvaz@users.noreply.github.com>
2026-04-14 15:48:50 -0300

    Adicionando lógica de admin

--- frontend/app/(pages)/chat/components/MenuMobile/MenuMobile.tsx
+++ frontend/app/(pages)/chat/components/MenuMobile/MenuMobile.tsx
@@ -8,16 +8,19 @@ interface MenuMobileProps {
   onDash?: () => void;
   onConfig?: () => void;
   onSair?: () => void;
+  isAdmin?: boolean;
 }
 
-export default function MenuMobile({ onNovoChat, onDash, onConfig, onSair }: MenuMobileProps){
+export default function MenuMobile({ onNovoChat, onDash, onConfig, onSair, isAdmin=false }: MenuMobileProps){    
     return(
         <>
             <section className={styles.MenuMobileConteiner}>
                 <article className={styles.MenuMobileArticle}>
                     <h3>Tutor AI</h3>
                     <button onClick={onNovoChat}><MessageCircleMore/>Novo Chat</button>
+                    {isAdmin && ( 
                     <button onClick={onDash}><Bolt/>Dashboard</button>
+                    )}
                     <button onClick={onConfig}><Bolt/>Configurações</button>
                     <button onClick={onSair}><Ban/>Sair</button>
                 </article>
