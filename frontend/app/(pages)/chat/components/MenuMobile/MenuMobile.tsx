a0dc36fd7463c38e8d1edad31aa2001431273b33
Ivin Vaz Braga Santos <133714762+ivinvaz@users.noreply.github.com>
2026-04-14 15:58:55 -0300

    Adicionando funcionalidade de swipe up no menu mobile para fechá-lo

--- frontend/app/(pages)/chat/components/MenuMobile/MenuMobile.tsx
+++ frontend/app/(pages)/chat/components/MenuMobile/MenuMobile.tsx
@@ -2,6 +2,7 @@
 
 import styles from "./MenuMobile.module.css";
 import { Ban, Bolt, MessageCircleMore} from "lucide-react";
+import { useRef } from "react";
 
 interface MenuMobileProps {
   onNovoChat?: () => void;
@@ -12,21 +13,36 @@ interface MenuMobileProps {
   onFechar?: () => void;
 }
 
-export default function MenuMobile({ onNovoChat, onDash, onConfig, onSair, isAdmin=false }: MenuMobileProps){    
+export default function MenuMobile({ onNovoChat, onDash, onConfig, onSair, isAdmin=false, onFechar }: MenuMobileProps){    
+    const startY = useRef(0);
+
+    const handleTouchStart = (e: React.TouchEvent) => {
+        startY.current = e.touches[0].clientY;
+    };
+
+    const handleTouchEnd = (e: React.TouchEvent) => {
+        const endY = e.changedTouches[0].clientY;
+        const diff = startY.current - endY;
+
+        if (diff > 50) {
+            onFechar?.();
+        }
+    };
+
     return(
-        <>
-            <section className={styles.MenuMobileConteiner}>
-                <article className={styles.MenuMobileArticle}>
-                    <h3>Tutor AI</h3>
-                    <button onClick={onNovoChat}><MessageCircleMore/>Novo Chat</button>
-                    {isAdmin && ( 
-                    <button onClick={onDash}><Bolt/>Dashboard</button>
-                    )}
-                    <button onClick={onConfig}><Bolt/>Configurações</button>
-                    <button onClick={onSair}><Ban/>Desconectar</button>
-                    <button onClick={onFechar}><Minimize2 />Fechar</button>
-                </article>
-            </section>
-        </>
+        <section 
+        className={styles.MenuMobileConteiner}
+        onTouchStart={handleTouchStart}
+        onTouchEnd={handleTouchEnd}>
+            <article className={styles.MenuMobileArticle}>
+                <h3>Tutor AI</h3>
+                <button onClick={onNovoChat}><MessageCircleMore/>Novo Chat</button>
+                {isAdmin && ( 
+                <button onClick={onDash}><Bolt/>Dashboard</button>
+                )}
+                <button onClick={onConfig}><Bolt/>Configurações</button>
+                <button onClick={onSair}><Ban/>Desconectar</button>
+            </article>
+        </section>
     )
 }
