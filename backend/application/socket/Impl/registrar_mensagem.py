32bd816edde244ea4a177aee8cf9a3f953628a94
Ivin Vaz Braga Santos <133714762+ivinvaz@users.noreply.github.com>
2026-04-14 17:10:36 -0300

    Atualizando registarmensagem

--- backend/application/socket/Impl/registrar_mensagem.py
+++ backend/application/socket/Impl/registrar_mensagem.py
@@ -1,13 +1,15 @@
 from application.config.database import db
 from application.models.model_mensagem import Mensagem
-
+from datetime import datetime
 
 def registrar_mensagem(id_chat, usuario_id, sessao_id, data_de_envio, conteudo):
     try:
         if not conteudo:
             print(f"[Warning] problema ao registrar mesnagem, conteudo vazio")
-            return
+            raise ValueError("Conteúdo da mensagem vazio")
         
+        data_de_envio = datetime.fromisoformat(data_de_envio.replace("Z", "+00:00"))
+
         nova_mensagem = Mensagem(
             chat_id=id_chat,
             sessao_id=sessao_id,
@@ -24,4 +26,4 @@ def registrar_mensagem(id_chat, usuario_id, sessao_id, data_de_envio, conteudo):
     except Exception as e:
         db.session.rollback()
         print(f"[Error] erro ao persistir mensagem: {e}")
-        return None
+        raise
