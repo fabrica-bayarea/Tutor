dc99d38631c7624141ca54b145f19764b3049a16
Ivin Vaz Braga Santos <133714762+ivinvaz@users.noreply.github.com>
2026-04-14 15:01:58 -0300

    Criando rota de data

---
+++ backend/application/routes/route_data.py
@@ -0,0 +1,29 @@
+from flask import Blueprint, jsonify, g
+from application.auth.auth_decorators import token_obrigatorio
+from application.services.service_data import buscar_materias_user, buscar_turmas_user
+
+chats_bp = Blueprint('data', __name__)
+
+@chats_bp.route('/materias', methods=['GET'])
+@token_obrigatorio
+def obter_materias_user():
+    try:
+        user_id = g.user_id
+        materias = buscar_materias_user(user_id=user_id)
+    except ValueError as e:
+        print(f'Erro ao buscar matérias: {str(e)}')
+        return jsonify({"error": str(e)}), 400
+    
+    return jsonify(materias), 200
+
+@chats_bp.route('/turmas', methods=['GET'])
+@token_obrigatorio
+def obter_turmas_user():
+    try:
+        user_id = g.user_id
+        turmas = buscar_turmas_user(user_id=user_id)
+    except ValueError as e:
+        print(f'Erro ao buscar turmas: {str(e)}')
+        return jsonify({"error": str(e)}), 400
+    
+    return jsonify(turmas), 200
