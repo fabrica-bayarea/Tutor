1d4a52eceaf462e7bda5b7f28c5f90217de313c4
Ivin Vaz Braga Santos <133714762+ivinvaz@users.noreply.github.com>
2026-04-16 14:31:19 -0300

    Realizando a integração com o wait_ollama

--- backend/application/mcp/llm/orchestrator.py
+++ backend/application/mcp/llm/orchestrator.py
@@ -1,19 +1,63 @@
 import httpx
+import asyncio
 
 class ModelOrchestrator:
 
     def __init__(self, registry):
         self.registry = registry
 
-    async def preload_models(self):
+    async def wait_until_ready(self):
+        await self._wait_ollama()
+        await self._pull_models()
+        await self._verify_models()
 
+    async def _wait_ollama(self):
+        url = "http://ollama-service:11434/api/tags"
+
+        while True:
+            try:
+                async with httpx.AsyncClient() as client:
+                    response = await client.get(url)
+
+                    if response.status_code == 200:
+                        print("Ollama disponível!")
+                        return
+
+            except Exception:
+                pass
+
+            print("Aguardando ollama...")
+            await asyncio.sleep(2)
+
+    async def _pull_models(self):
         for llm_id, config in self.registry._models.items():
-            if config["provider"] == "ollama":
-                await self._pull_ollama_model(config["model_name"])
 
-    async def _pull_ollama_model(self, model_name:str):
+            if config["provider"] != "ollama":
+                continue
+
+            model_name = config["model_name"]
+
+            print(f"Garantindo modelo: {model_name}")
+
+            async with httpx.AsyncClient(timeout=None) as  client:
+                await client.post(
+                    "http://ollama-service:11434/api/pull",
+                    json={"name": model_name}
+                )
+
+    async def _verify_models(self):
         async with httpx.AsyncClient() as client:
-            await client.post(
-                "http://localhost:11434/api/pull",
-                json={"name": model_name}
-            )
+
+            r = await client.get("http://ollama-service:11434/api/tags")
+            data = r.json()
+
+            available = {m["name"] for m in data.get("models", [])}
+
+            for llm_id, config in self.registry._models.items():
+                model = config["model_name"]
+
+                if model not in available:
+                    raise Exception(f"Modelo {model} não carregado no Ollama")
+
+            print("Todos os modelos estão prontos!")
+    
