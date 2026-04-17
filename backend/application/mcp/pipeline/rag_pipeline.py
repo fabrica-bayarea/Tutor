ac40109afd2413c7a1d055c7643cbe2756b5a835
Ivin Vaz Braga Santos <133714762+ivinvaz@users.noreply.github.com>
2026-04-17 14:47:34 -0300

    Atualizando ragpipeline

--- backend/application/mcp/pipeline/rag_pipeline.py
+++ backend/application/mcp/pipeline/rag_pipeline.py
@@ -3,6 +3,7 @@ from application.mcp.llm.ollama_client import OllamaClient
 from application.mcp.tools.busca_semantica_tool import busca_semantica
 from application.mcp.core.prompt_builder import build_prompt
 from application.models import Materia
+from application.socket.Impl.disparar_emit import disparar_emit
 
 class RAGPipeline:
 
@@ -11,21 +12,13 @@ class RAGPipeline:
         self.registry = registry
         self.ollama = ollama_client
 
-    async def run_stream(self,materia_id:str, pergunta:str,historico:str, socketio, sid):
+    async def run_stream(self,materia_id:str, pergunta:str,historico:str, socketio, sid, model:str, materia: str):
 
-        contexto = await busca_semantica(materia_id,pergunta)
+        contexto = await busca_semantica(materia_id,pergunta,sid,socketio)
 
-        model_config = self.router.resolve(materia_id)
+        disparar_emit(socketio, 'construindo_prompt',{}, sid)
+        prompt = build_prompt(materia, contexto, historico, pergunta)
 
-        materia = Materia.query.filter_by(id=materia_id).first()
-        nome_materia = materia.nome if materia else materia_id
-
-        prompt = build_prompt(nome_materia, contexto, historico, pergunta)
-
-        async for chunk in self.ollama.stream_generate(
-            model_config["model_name"],
-            prompt
-        ):
-            socketio.emit("chunk_mensagem", {"data": chunk}, to=sid)
+        async for chunk in self.ollama.stream_generate(model,prompt):socketio.emit("chunk_mensagem", {"data": chunk}, to=sid)
 
         socketio.emit("processo_completo", {"status": "done"}, to=sid)
