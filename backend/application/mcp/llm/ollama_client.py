f06a7ff721a7b2fc6175b6d1f118e44c3311e4e4
Ivin Vaz Braga Santos <133714762+ivinvaz@users.noreply.github.com>
2026-04-22 15:58:40 -0300

    Atualizando ollama_client.py

--- backend/application/mcp/llm/ollama_client.py
+++ backend/application/mcp/llm/ollama_client.py
@@ -5,28 +5,6 @@ import os
 BASE_URL = os.getenv("OLLAMA_HOST", "http://localhost:11434")
 
 class OllamaClient:
-
-    # Este método lista a llm registrada na matéria selecionada
-    async def list_models(self):
-        async with httpx.AsyncClient() as client:
-            response = await client.get(f"{BASE_URL}/api/tags")
-            data = response.json()
-            return [materia["name"] for materia in data.get("models",[])]
-        
-    # Este método gera uma resposta com a LLM de escolha com stream falso    
-    async def generate(self,model:str, prompt:str) -> str:
-        async with httpx.AsyncClient() as client:
-            response = await client.post(
-                f"{BASE_URL}/api/generate",
-                json={
-                    "model":model,
-                    "prompt":prompt,
-                    "stream":False
-                }
-            )
-            return response.json().get("response","")
-    
-    # Este método gera uma resposta com a LLM de escolha com stream verdadeiro
     async def stream_generate(self, model: str, prompt: str):
         async with httpx.AsyncClient(timeout=None) as client:
             async with client.stream(
