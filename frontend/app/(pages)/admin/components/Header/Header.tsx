87c3863a053b5cdf87d2efe55e50407e3264ba99
Ivin <vaz.ivin@gmail.com>
2026-04-26 20:31:05 -0300

    Adicionando contexto ao header de admins

--- frontend/app/(pages)/admin/components/Header/Header.tsx
+++ frontend/app/(pages)/admin/components/Header/Header.tsx
@@ -3,9 +3,10 @@ import { User, Bell, Menu } from "lucide-react";
 import UrlChanfro from "./components/UrlChanfro";
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
+import { useAuth } from '@/utils/auth';
 
 export default function Header(){
-    const user = "usuario"
+    const { user, isStudent, isProfessor, isAdmin } = useAuth();
     const url = usePathname();
     const [caminho, setCaminho] = useState("");
     
@@ -22,7 +23,7 @@ export default function Header(){
                     <Menu size={24}/>
                     <p>{caminho}</p>
                 </section>
-                <p>ADM - {user}</p>
+                <p>ADM - {user?.nome}</p>
                 <button className={styles.bellButton}><Bell size={20} color="white"/></button>
                 <button className={styles.userButton}><User size={20} color="#0F766E"/></button>
             </section>
