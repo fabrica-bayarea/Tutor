e0681df720b760da248af10709c21b24bdd069af
Ivin Vaz Braga Santos <133714762+ivinvaz@users.noreply.github.com>
2026-05-05 16:10:02 -0300

    Adicionando links de chats do banco de dados

--- frontend/app/(pages)/chat/layout.tsx
+++ frontend/app/(pages)/chat/layout.tsx
@@ -1,25 +1,51 @@
 'use client';
 
-import { useContext, useState } from "react"
+import { useContext, useState, useEffect } from "react"
 import styles from "./layout.module.css"
 import Aside from "./components/Aside/Aside";
 import { useRouter } from "next/navigation";
 import { LayoutContext } from "@/contexts/LayoutContext";
+import { obterChats, obterMateriaId } from "@/app/services/service_chat";
+import { useAuth } from "@/utils/auth";
+import { obterMateria } from "@/app/services/service_materia";
 
 export default function AlunoLayout({ children }: { children: React.ReactNode }) {
     const [chatKey, setChatKey] = useState(0)
     const router = useRouter()
     const { isMenuMobileAberto, setIsMenuAbertoMobile } = useContext(LayoutContext)!;
+    const { user } = useAuth();
+    const [chatLinks, setChatLinks] = useState([]);
 
     const handleNewChat = ()=>{
-        setChatKey(prev => prev + 1)
+        setChatKey((prev: number) => prev + 1)
         if (window.innerWidth <= 768) setIsMenuAbertoMobile(false)
         router.push("/chat")
     }
 
+    const getData = async () => {
+        if (!user?.id) return;
+        const chats = await obterChats(user?.id)
+
+        const limitedChats = chats.slice(0, 5);
+
+        const links = await Promise.all(
+            limitedChats.map(async (el) => {
+                const materiaId = await obterMateriaId(el.id);
+                const materiaNome = await obterMateria(materiaId);
+                return { id: el.id, materia: materiaNome, nome: el.nome };
+            })
+        );
+
+        setChatLinks(links);
+    }
+
+    useEffect(()=>{
+        getData();
+    },[user])
+
     return (
         <section className={styles.mainSection}>
-            <Aside links={[{"id":"1","materia":"Matemática","nome":"Quanto é 1 mais um?"}]}  onNewChat={handleNewChat}/>
+            <Aside links={chatLinks}  onNewChat={handleNewChat}/>
             <section className={styles.pageMediaSection}>
                 <section key={chatKey}>
                     {children}    
