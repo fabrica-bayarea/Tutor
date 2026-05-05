6c89dfe180c79f65e1f59a9464ecbae5ee0ce6a8
Ivin Vaz Braga Santos <133714762+ivinvaz@users.noreply.github.com>
2026-05-05 15:57:29 -0300

    Adicionando histórico de mensagens

--- frontend/app/(pages)/chat/(pages)/[id]/historico/page.tsx
+++ frontend/app/(pages)/chat/(pages)/[id]/historico/page.tsx
@@ -1,21 +1,36 @@
 "use client"
 
 import { useState, useRef, useEffect } from "react";
+import { useParams } from "next/navigation";
 import Header from "../../../components/Header/Header";
 import MessageField, {MessageFieldRef} from "../../../components/MessageField/MessageField";
-import { mockMessages } from "../../../utils/prompts";
+import { obterMateriaId } from "@/app/services/service_chat";
+import { obterMateria } from "@/app/services/service_materia";
+import { obterMensagens } from "@/app/services/service_mensagem";
 
 export default function Historico(){
+    const params = useParams();
+    const id = params.id as string;
+
     const [temMensagem, setTemMensagem] = useState(true)
     const messageFieldRef = useRef<MessageFieldRef>(null);
-    const [materia, setMateria] = useState("Matemática")
+    const [materia, setMateria] = useState("")
 
-    useEffect(()=>{
-        mockMessages.forEach((el)=>{
-            if(el["type"] == "user") messageFieldRef.current?.addMessage("user",el["message"])
-            else messageFieldRef.current?.addMessage("llm",el["message"])
-        })
-    },[])
+    useEffect(async ()=>{
+        const carregarDados = async () => {
+            let idMateria = await obterMateriaId(id);
+            let registro_materia = await obterMateria(idMateria);
+            setMateria(registro_materia["nome"]);
+
+            let messages = await obterMensagens(id);
+            messages.forEach((el) => {
+                messageFieldRef.current?.addMessage(el["sender_type"], el["conteudo"]);
+            });
+        };
+
+        carregarDados();
+    },[id]);
+    
     return(
         <>
             <Header isSelectInactive={temMensagem} materiaName={materia}/>
