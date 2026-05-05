60d91fe1cdc61f4280aad40d3920cc73ee5c135a
Ivin Vaz Braga Santos <133714762+ivinvaz@users.noreply.github.com>
2026-05-05 14:41:58 -0300

    Alterando prompt

--- backend/application/socket/Impl/prompt_builder.py
+++ backend/application/socket/Impl/prompt_builder.py
@@ -6,8 +6,7 @@ def build_prompt(materia: str, contexto: list[str], historico: str, pergunta: st
 [SYSTEM]
 Você é um tutor especialista em {materia}. 
 Responda de forma estretruturada e educativa, formatada em markdown(com listas, negrito, título, etc) e em português a pergunta apresentada abaixo. 
-Se a sessão de contexto deste prompt não estiver vazia, cite explicitamente trechos dela como fonte ao final da resposta. 
-Caso a sessão de contexto esteja vazia, cite fontes internas do modelo ao final da resposta.
+Use a sessão de contexto neste prompt como base da sua resposta e cite explicitamente trechos dela como fonte ao final da resposta. 
 
 [CONTEXT]
 {contexto_txt}
