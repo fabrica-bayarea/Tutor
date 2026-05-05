e9765aa5eb1bec3a801853dfe1f172bb3b27cf1d
Ivin Vaz Braga Santos <133714762+ivinvaz@users.noreply.github.com>
2026-05-05 15:23:22 -0300

    Update constants.ts

--- frontend/constants.ts
+++ frontend/constants.ts
@@ -1,5 +1,3 @@
-export const LLM_UUID = "ae5905d6-cb44-4884-9408-6fcd7cb3b27c"
-
 export const TIPOS_ARQUIVO = {
     "documento": [".pdf", ".docx", ".pptx", ".xlsx", ".csv", ".html", ".xhtml", ".md", ".markdown"],
     "video": [".mp4", ".mov", ".mkv", ".avi"],
