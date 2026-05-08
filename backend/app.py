b7dc8841bd079b5bd6deb9d8d7ea8fad94494d73
DavidLBO <davidlopes009@gmail.com>
2026-05-07 22:04:07 -0300

    [UD-03] Adiciona blueprint para /auth

--- backend/app.py
+++ backend/app.py
@@ -5,6 +5,7 @@ from flask_cors import CORS
 from application.config.config import Config
 from application.config.database import init_db, db
 from application.socket.socket_instance import socketio
+from application.routes.route_auth import auth_bp
 
 import application.socket.event_handler
 
@@ -55,6 +56,7 @@ app.register_blueprint(mensagens_bp, url_prefix="/mensagens")
 app.register_blueprint(vinculos_bp, url_prefix="/vinculos")
 app.register_blueprint(data_bp, url_prefix="/data")
 app.register_blueprint(admin_bp, url_prefix="/admin")
+app.register_blueprint(auth_bp, url_prefix='/auth')
 
 if __name__ == "__main__":
     socketio.run(app, host="0.0.0.0", port=5000)
