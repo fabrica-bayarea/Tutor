a444e16698fe6c548fe207fe7b4743335080e6c1
DavidLBO <davidlopes009@gmail.com>
2026-05-19 22:53:36 -0300

    [US-38] Adiciona status em model_llm

--- backend/application/models/model_llm.py
+++ backend/application/models/model_llm.py
@@ -4,3 +4,10 @@ class LLM(db.Model):
     __tablename__ = "llm"
 
     model_id = db.Column(db.String, primary_key=True)
+    
+    status = db.Column(
+        db.Enum('ativada', 'desativada', name='llmstatusenum', native_enum=False),
+        nullable=False,
+        default='desativada',
+        server_default='desativada',
+    )
