e8c5dbe4b98d25d72581f43fa0aa2e02a70320ef
Ivin Vaz Braga Santos <133714762+ivinvaz@users.noreply.github.com>
2026-05-25 15:12:57 -0300

    Atuliazando config.py

--- backend/application/config/config.py
+++ backend/application/config/config.py
@@ -1,10 +1,7 @@
 import os
-from pathlib import Path
 from dotenv import load_dotenv
 
-# Caminho absoluto para o .env na raiz de backend
-dotenv_path = Path(__file__).resolve().parents[2] / ".env"
-load_dotenv(dotenv_path)
+load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY")
